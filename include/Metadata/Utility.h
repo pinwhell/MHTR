2d6a323d24494cad102e802344da5c02b24aa7ed
Pinwhell <60289470+pinwhell@users.noreply.github.com>
2024-06-10 08:54:15 -0400

    Decl/Def Separations

--- include/Metadata/Utility.h
+++ include/Metadata/Utility.h
@@ -1,38 +1,11 @@
 #pragma once
 
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iterator>
 #include <set>
+#include <string>
 
 #include <Metadata/Container.h>
-#include <Metadata/Target.h>
 
 using NamespaceSet = std::set<std::string>;
 
-static NamespaceMetadataTargetSetMap NsMultiMetadataMapFromMultiMetadata(const MetadataTargetSet& targets)
-{
-	NamespaceMetadataTargetSetMap result;
-
-	for (auto* target : targets)
-	{
-		const INamespace* targetNs = target->mFullIdentifier.mNamespace;
-		std::string targetNsStr = targetNs ? targetNs->GetNamespace() : METADATA_NS_NULL;
-		result[targetNsStr].insert(target);
-	}
-
-	return result;
-}
-
-static NamespaceSet AllNsFromMultiMetadataTarget(const MetadataTargetSet& targets)
-{
-	NamespaceSet result;
-
-	std::transform(targets.begin(), targets.end(), std::inserter(result, result.end()), [](MetadataTarget* target) {
-		INamespace* ns = target->mFullIdentifier.mNamespace;
-		return ns ? ns->GetNamespace() : METADATA_NS_NULL;
-		});
-
-	return result;
-}
+NamespaceMetadataTargetSetMap NsMultiMetadataMapFromMultiMetadata(const MetadataTargetSet& targets);
+NamespaceSet AllNsFromMultiMetadataTarget(const MetadataTargetSet& targets);
---
+++ src/Metadata/Utility.cpp
@@ -0,0 +1,29 @@
+#include <Metadata/Utility.h>
+#include <algorithm>
+#include <iterator>
+
+NamespaceMetadataTargetSetMap NsMultiMetadataMapFromMultiMetadata(const MetadataTargetSet& targets)
+{
+	NamespaceMetadataTargetSetMap result;
+
+	for (auto* target : targets)
+	{
+		const INamespace* targetNs = target->mFullIdentifier.mNamespace;
+		std::string targetNsStr = targetNs ? targetNs->GetNamespace() : METADATA_NS_NULL;
+		result[targetNsStr].insert(target);
+	}
+
+	return result;
+}
+
+NamespaceSet AllNsFromMultiMetadataTarget(const MetadataTargetSet& targets)
+{
+	NamespaceSet result;
+
+	std::transform(targets.begin(), targets.end(), std::inserter(result, result.end()), [](MetadataTarget* target) {
+		INamespace* ns = target->mFullIdentifier.mNamespace;
+		return ns ? ns->GetNamespace() : METADATA_NS_NULL;
+		});
+
+	return result;
+}
